75ba7a39c55fe6bac9c417c971b476b9679fef55
DishantDrugkar <dishantdrugkar4@gmail.com>
2025-05-03 21:45:57 +0530

    DOM

---
+++ JavaScript/DOM/dom.js
@@ -0,0 +1,17 @@
+// let heading =  document.getElementById("heading");
+// console.log(heading);
+
+// let city = document.getElementsByClassName("city");
+// console.dir(city);
+// console.log(city);
+
+
+// ---------------------------------------------------------------------------------
+
+// querySelector
+
+let city = document.querySelector("h2");  // first element
+console.dir(city);
+
+let city2 = document.querySelectorAll("h2");  // first element
+console.dir(city2);
